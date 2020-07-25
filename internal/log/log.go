8f5cc3146e7ea5dbd8ec7523a3ed56f8a7aa9944
Disco <developer@onkraken.net>
2020-07-25 16:49:15 +0100

    Added fatal logging

--- internal/log/log.go
+++ internal/log/log.go
@@ -30,3 +30,12 @@ func Info(logLine string) {
 func Error(logLine string) {
 	log.WriteString(fmt.Sprintf("%s ERROR %s \n", time.Now().Format(time.UnixDate), logLine))
 }
+
+func Errorf(format string, args ...interface{}) {
+	log.WriteString(fmt.Sprintf("%s ERROR %s %s \n", time.Now().Format(time.UnixDate), fmt.Sprintf(format, args...)))
+}
+
+func Fatalf(format string, args ...interface{}) {
+	log.WriteString(fmt.Sprintf("%s FATAL %s %s \n", time.Now().Format(time.UnixDate), fmt.Sprintf(format, args...)))
+	os.Exit()
+}
