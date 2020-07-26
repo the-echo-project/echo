844ad4ff0031328438890e0da94acbc4091d7829
Disco <developer@onkraken.net>
2020-07-26 15:44:18 +0100

    Added userCmd flags

--- cmd/user.go
+++ cmd/user.go
@@ -1,1 +1,40 @@
 package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/the-echo-project/echo/api"
+)
+
+var(
+	userCmd = &cobra.Command{
+		Use: "user",
+		Short: "Perform user commands.",
+		Long: "Perform user commands.",
+	}
+
+	UserFlags UserCmdFlag
+)
+
+type UserCmdFlag struct {
+	Create bool
+	Remove bool
+	Update bool
+	DataJSON string
+	DataFlags api.User
+}
+
+func init() {
+	userCmd.Flags().BoolVarP(&UserFlags.Create, "create", "c",false, "Create a new user (use with the -d flag)")
+	userCmd.Flags().BoolVarP(&UserFlags.Remove, "remove", "r", false, "Remove a user (use with the -d flag)")
+	userCmd.Flags().BoolVarP(&UserFlags.Update, "update", "u", false, "Update an existing user (use with the -d flag)")
+
+	// API query data in JSON format
+	userCmd.Flags().StringVarP(&UserFlags.DataJSON,"data", "d", "", "Specify user JSON")
+
+	// Simpler flag formed data
+	userCmd.Flags().StringVarP(&UserFlags.DataFlags.Email, "email", "e", "", "User email")
+	userCmd.Flags().StringVarP(&UserFlags.DataFlags.Username, "username", "n", "", "Username")
+	userCmd.Flags().StringVarP(&UserFlags.DataFlags.FirstName, "first", "f", "", "User first name")
+	userCmd.Flags().StringVarP(&UserFlags.DataFlags.LastName, "last", "l", "", "User last name")
+	userCmd.Flags().StringVarP(&UserFlags.DataFlags.Password, "password", "p", "", "User password")
+}
