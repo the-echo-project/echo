774341f9e17d6c5a255a704369b7fe0be25c24fa
Disco <developer@onkraken.net>
2020-11-18 00:41:12 +0000

    Added user_id to GET response, updated User model user_id to be string

--- api/models/user.go
+++ api/models/user.go
@@ -3,7 +3,7 @@ package models
 import "database/sql"
 
 type User struct {
-	UserID []byte `json:"user_id" db:"user_id"`
+	UserID string `json:"user_id" db:"user_id"`
 	FirstName string `json:"first_name" db:"first_name"`
 	LastName string `json:"last_name" db:"last_name"`
 	Username string `json:"username" db:"username"`
--- api/user.go
+++ api/user.go
@@ -33,7 +33,7 @@ func getMyUser(w http.ResponseWriter, r *http.Request) {
 
 func sendUserEntryFromDB(w http.ResponseWriter, userID string) {
 	var u models.User
-	err := db.EchoDB.QueryRowx("SELECT first_name, last_name, username, email, last_lifesign, avg_lifesign_interval, roles FROM users WHERE user_id = $1", userID).StructScan(&u)
+	err := db.EchoDB.QueryRowx("SELECT user_id, first_name, last_name, username, email, last_lifesign, avg_lifesign_interval, roles FROM users WHERE user_id = $1", userID).StructScan(&u)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		log.This.Warning(err.Error())
@@ -57,11 +57,12 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if _, err := db.EchoDB.Query("INSERT INTO users (first_name, last_name, username, email, roles, password) values ($1, $2, $3, $4, $5, crypt($6, gen_salt('bf')))", u.FirstName, u.LastName, u.Username, u.Email, nil, u.Password); err != nil {
+	if err := db.EchoDB.QueryRowx("INSERT INTO users (first_name, last_name, username, email, roles, password) values ($1, $2, $3, $4, $5, crypt($6, gen_salt('bf'))) RETURNING user_id", u.FirstName, u.LastName, u.Username, u.Email, nil, u.Password).StructScan(&u); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		log.This.Warning(err.Error())
 		return
 	}
+	sendUserEntryFromDB(w, u.UserID)
 }
 
 func updateUser(w http.ResponseWriter, r *http.Request) {
